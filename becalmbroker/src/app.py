114047d6fc499d75fc84c64faba5258e6cebaadc
Enrique Melero <enrique.melero@gmail.com>
2020-04-09 19:12:53 +0200

    Added broker access to  max30100

--- becalmbroker/src/app.py
+++ becalmbroker/src/app.py
@@ -13,8 +13,11 @@ import json
 from flask_apscheduler import APScheduler
 
 sensorurl="http://localhost:8888"
+sensorurl2="http://localhost:8887"
 temperature = "-1"
 pressure = "-1"
+heartbeat= "-1"
+SpO2= "-1"
 
 scheduler = APScheduler()
 
@@ -42,19 +45,55 @@ def job1():
             print "Error: " + str(r.status_code)
             return
 
+        # Gather data from pulsioximeter
+        r = requests.get(sensorurl2 + '/rest/api/v1.0/heartbeat')
+
+        if r.status_code == 200:
+            print "HBR returns: " + r.text
+            heartbeat=r.text
+        else:
+            print "Error: " + str(r.status_code)
+            return
+
+        r = requests.get(sensorurl2 + '/rest/api/v1.0/spo2')
+
+        if r.status_code == 200:
+            print "Press returns: " + r.text
+            SpO2=r.text
+        else:
+            print "Error: " + str(r.status_code)
+            return
+
+
+
     tempJ = {}
     presJ = {}
+    hbrJ= {}
+    spo2= {}
     tempJ['measure_type'] = 't'
     tempJ['measure_value'] = temperature.__str__()
     tempJ['date_generation'] = datetime.now().__str__()
+
     presJ['measure_type'] = 'p'
     presJ['measure_value'] = pressure.__str__()
     presJ['date_generation'] = datetime.now().__str__()
-    # Post results to central server
+
+    hbrJ['measure_type'] = 'h'
+    hbrJ['measure_value'] = heartbeat.__str__()
+    hbrJ['date_generation'] = datetime.now().__str__()
+ 
+    spo2J['measure_type'] = 'o'
+    spo2J['measure_value'] = spo2.__str__()
+    spo2J['date_generation'] = spo2.now().__str__()
+ 
+
+
+# Post results to central server
     payload = []
     payload.append(tempJ)
     payload.append(presJ)
-
+    payload.append(hbrJ)
+    payload.append(spo2J)
     print json.dumps(payload) ;
     headers = {'Content-type': 'application/json'}
     r = requests.post('http://becalm.ngrok.io/data-sensor/2?id_device=1', headers=headers, json=payload) 
